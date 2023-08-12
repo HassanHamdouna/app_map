f93ba9e7da7df634a92b3f665a3a5d25585a5679
HassanHamdouna <hassan.h.developer@gmail.com>
2023-08-12 10:20:13 +0300

    add google map for ios and androiad

--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,12 +9,21 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController _googleMapController;
+  CameraPosition _initialCameraPosition =
+      CameraPosition(target: LatLng(31.515483, 34.439996), zoom: 16);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Map'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: _initialCameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _googleMapController = controller;
+        },
       ),
     );
   }
