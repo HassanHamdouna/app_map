9d3def6a5cb05e01bbf6ea7bd523da01a681457f
HassanHamdouna <hassan.h.developer@gmail.com>
2023-08-13 21:58:38 +0300

    add library material_floating_search_bar

--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -2,6 +2,7 @@ import 'package:app_map/helpers/location_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -41,6 +42,7 @@ class _MapScreenState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        fit: StackFit.expand,
         children: [
           position == null
               ? const Center(
@@ -48,6 +50,8 @@ class _MapScreenState extends State<MapScreen> {
                 color: Colors.blue,
               ))
               : buildMap(),
+          buildFloatingSearchBar(),
+
         ],
       ),
       floatingActionButton: Container(
@@ -114,5 +118,51 @@ class _MapScreenState extends State<MapScreen> {
     setState(() {
       markes.add(newMarker);
     });
+  }
+
+  Widget buildFloatingSearchBar() {
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+      },
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            /*child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),*/
+          ),
+        );
+      },
+    );
   }
 }
